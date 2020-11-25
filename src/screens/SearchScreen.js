d2b38a6af49ff8f9e6aa33550d559e5083b17e6e
Jonathan F. K <jonathanfk1@gmail.com>
2020-11-24 22:02:48 -0300

    Food App Test Initial Commit.

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../componens/SearchBar";
+
+const SearchScreen = () => {
+    return <View>
+        <SearchBar />
+        <Text>Search Screen</Text>
+    </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
