d256342c3481228c625d9229266d7270b1d78eee
Jonathan F. K <jonathanfk1@gmail.com>
2020-12-28 20:47:16 -0300

    Error handling.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,41 @@
 import React, {useState} from 'react';
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../componens/SearchBar";
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async (searchTerm) => {
+        console.log('Hi there');
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses);
+        } catch (e) {
+            setErrorMessage('Something went wrong');
+        }
+    };
+
+    // Call searchApi when component is first rendered <- BAD CODE
+    // searchApi('pasta');
 
     return (
         <View>
             <SearchBar
                 term={term}
-                onTermChange={newTerm => setTerm(newTerm)}
-                onTermSubmit={() => console.log('term was submitted')}
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(term)}
             />
-            <Text>Search Screen</Text>
-            <Text>{term}</Text>
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found {results.length} results</Text>
         </View>
     );
 };
