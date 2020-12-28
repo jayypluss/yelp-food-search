591e88c6e5d7e16a182d8c31fdcd92e4495bf9b8
Jonathan F. K <jonathanfk1@gmail.com>
2020-12-28 19:23:46 -0300

    Adding axios and starting Yelp integration.

--- src/componens/SearchBar.js
+++ src/componens/SearchBar.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from "react-native";
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
     return (
         <View style={styles.background}>
             <Feather name="search" style={styles.iconStyle} />
             <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
                 style={styles.inputStyle}
                 placeholder="Search"
+                value={term}
+                onChangeText={onTermChange}
+                onEndEditing={onTermSubmit}
             />
         </View>
     );
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../componens/SearchBar";
 
 const SearchScreen = () => {
-    return <View>
-        <SearchBar />
-        <Text>Search Screen</Text>
-    </View>
+    const [term, setTerm] = useState('');
+
+    return (
+        <View>
+            <SearchBar
+                term={term}
+                onTermChange={newTerm => setTerm(newTerm)}
+                onTermSubmit={() => console.log('term was submitted')}
+            />
+            <Text>Search Screen</Text>
+            <Text>{term}</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({});
