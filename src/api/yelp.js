b289e1e42697ff80e72affa7bb15eb311bf2289d
Jonathan F. K <jonathanfk1@gmail.com>
2020-12-29 07:37:28 -0300

    Removing token.

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer otE9ANTu04ZXQpA5-JLG2O09kymLeGu-dByPlPILeTjszBnG5sX1NuoavAN2MFWZ9G-7q5lX2OjS21_8DMsAi645maGPfgPcjaYT6nKNS1SE87ByHCqmT5WBxn69X3Yx'
+        Authorization: 'Bearer '
     }
 });
